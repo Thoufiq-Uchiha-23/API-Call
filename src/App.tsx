20bb5a7f5cb1a0967ffa2523be300b9e80a6795e
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2025-03-20 22:28:55 +0530

    api call fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface TodoItem {
+  completed: boolean;
+  id: number;
+  title: string;
+  userId: number;
 }
 
-export default App
+function App() {
+  const [todos, setTodos] = useState<TodoItem[]>([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((response) => response)
+      .then((e) => e.json())
+      .then((e) => setTodos(e));
+  }, []);
+  return <div className="App">
+    {todos.map((todo) => {
+      return <li key={todo.id}>{todo.title}</li>
+    })}
+  </div>;
+}
+
+export default App;
